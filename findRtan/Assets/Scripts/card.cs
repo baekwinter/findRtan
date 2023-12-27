6780a0f5363d1a10a886d2e2327a0ef71bc45558
baekwinter <kksbiy0012@gmail.com>
2023-12-27 20:19:36 +0900

    프로젝트_유니티
    4주차 과제 제출

---
+++ findRtan/Assets/Scripts/card.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void opencard()
+    {
+        anim.SetBool("isOpen", true);
+        // Animator isOpen = true
+        transform.Find("front").gameObject.SetActive(true);
+        // Front SetActive = true
+        transform.Find("back").gameObject.SetActive(false);
+        // back SetActive = false
+
+        if (gameManager.I.firstCard == null)
+        {
+            gameManager.I.firstCard = gameObject;
+        }
+        else
+        {
+            gameManager.I.secondCard = gameObject;
+            gameManager.I.isMatched();
+        }
+
+
+    }
+
+    public void destroyCard()
+    {
+        Invoke("destroyCardInvoke", 1.0f);
+    }
+
+    void destroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void closeCard()
+    {
+        Invoke("closeCardInvoke", 1.0f);
+    }
+
+    void closeCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        transform.Find("back").gameObject.SetActive(true);
+        transform.Find("front").gameObject.SetActive(false);
+    }
+}
